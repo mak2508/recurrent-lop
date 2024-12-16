2ecfa6457b6e28a86518efb21ce228188733d913
Ahmad Khan <ahkhan@ethz.ch>
2024-12-16 22:25:02 +0100

    Move nets to shared folder

---
+++ src/nets/__init__.py
@@ -0,0 +1,4 @@
+from .mlp import MLP
+from .lstm import LSTM
+
+__all__ = ["MLP", "LSTM"]
--- src/nets/lstm.py
+++ src/nets/lstm.py
@@ -1,30 +1,5 @@
 import torch.nn as nn
-
-
-# Define the MLP model
-class MLP(nn.Module):
-    def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_classes: int,
-        dropout_rate: float,
-    ) -> None:
-        super(MLP, self).__init__()
-        self.flatten = nn.Flatten()
-        self.model = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(hidden_size, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.flatten(x)
-        return self.model(x)
+import torch
 
 
 class LSTM(nn.Module):
