6415e9872270f264a29f65c937eef331781012e3
Ahmad Khan <ahkhan@ethz.ch>
2025-01-03 00:24:16 +0100

    Switch to abstract base class for algos

--- src/algos/algo_type.py
+++ src/algos/algo_type.py
@@ -1,8 +1,9 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
 import torch
 from typing import Union, Tuple
 
-class AlgoType(Protocol):
+class AlgoType(ABC):
+    @abstractmethod
     def learn(self, x: torch.Tensor, target: torch.Tensor) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
         """
         Learn using one step of the algorithm
@@ -10,4 +11,4 @@ class AlgoType(Protocol):
         :param target: desired output
         :return: loss or (loss, output)
         """
-        ...
+        pass
